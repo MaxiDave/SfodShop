9ed81d9e302a50f7b99922df6f7327c5621f20c1
MaxiDave13 <maxidave13@gmail.com>
2017-08-05 16:43:34 +0200

    Creada Sfod amb FXML

--- Sfod/src/sfod/FXMLDocumentController.java
+++ Sfod/src/sfod/FXMLDocumentController.java
@@ -1,38 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sfod;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-/**
- *
- * @author david
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    @FXML
-    private Button button;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btn1;
+    
+    @FXML
+    private Button btn2;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Stage stage; 
+        Parent root;
+        if(event.getSource()==btn1){
+            //get reference to the button's stage         
+            stage=(Stage) btn1.getScene().getWindow();
+            //load up OTHER FXML document
+            root = FXMLLoader.load(getClass().getResource("FXML2.fxml"));
+        }
+        else{
+            stage=(Stage) btn2.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
+        }
+        //create a new scene with root and set the stage
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
-    
 }

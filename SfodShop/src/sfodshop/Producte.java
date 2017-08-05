8ce7c81377f871e5cf1b4a1dc34eeb92e7348887
MaxiDave13 <maxidave13@gmail.com>
2017-08-05 15:41:21 +0200

    Inicial commit

---
+++ SfodShop/src/sfodshop/Producte.java
@@ -0,0 +1,14 @@
+package sfodshop;
+
+public class Producte {
+    private Integer EBAN;
+    private String codi;
+    private String descripcio;
+    
+    public Producte(Integer EBAN, String codi, String descripcio) {
+        this.EBAN= EBAN;
+        this.codi= codi;
+        this.descripcio= descripcio;
+    }
+    
+}

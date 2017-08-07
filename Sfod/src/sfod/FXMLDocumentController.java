af70d9117019f18a0bd2557afa46aa7cf8a7e367
MaxiDave <maxidave13@gmail.com>
2017-08-07 21:45:11 +0200

    Problemes per fer comandes a sql

--- Sfod/src/sfod/FXMLDocumentController.java
+++ Sfod/src/sfod/FXMLDocumentController.java
@@ -3,7 +3,10 @@ package sfod;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +17,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -38,14 +42,23 @@ public class FXMLDocumentController implements Initializable {
     private Button btn2;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException {
+    private Button sqlExecutar;
+    
+    @FXML
+    private TextArea sqlComanda;
+    
+    private Connection conexio;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException, SQLException {
         Stage stage; 
         Parent root;
         if(event.getSource()==loginButton){
             try {
                 //get reference to the button's stage
                 stage=(Stage) loginButton.getScene().getWindow();
-                Connection conexio= SQL.connectar(loginUser.getText(), loginPswd.getText());
+                conexio= SQL.connectar(loginUser.getText(), loginPswd.getText());
+                
                 //load up OTHER FXML document
                 root = FXMLLoader.load(getClass().getResource("FXML2.fxml"));
                 
@@ -54,16 +67,31 @@ public class FXMLDocumentController implements Initializable {
                 stage.setScene(scene);
                 stage.show();
             } catch (SQLException | ClassNotFoundException ex) {
+                System.out.println(ex);
                 loginError.setVisible(true);
             }
         }
-        else{
+        else if(event.getSource()==btn2){
             stage=(Stage) btn2.getScene().getWindow();
             root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
             //create a new scene with root and set the stage
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+        }
+        else{
+            Statement stmt = conexio.createStatement();
+            System.out.println(sqlComanda.getText());
+            ResultSet rs = stmt.executeQuery(sqlComanda.getPromptText());
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+            }
         }
     }
     
--- Sfod/src/sfod/SQL.java
+++ Sfod/src/sfod/SQL.java
@@ -6,13 +6,15 @@ import java.sql.SQLException;
 
 public abstract class SQL {
     private static final String server= "jdbc:mysql://sql11.freemysqlhosting.net/sql11186567";
-    //private static String user="sql11186567";
-    //private static String pass="fmyLWFTGUu";
+    private static String user="sql11186567";
+    private static String pass="fmyLWFTGUu";
     private static final String driver="com.mysql.jdbc.Driver";
     
     public static Connection connectar(String user, String pass) throws SQLException, ClassNotFoundException{
         Connection conexion;
         Class.forName(driver);
+        user= SQL.user;
+        pass= SQL.pass;
         conexion=DriverManager.getConnection(server, user, pass);
         return conexion;
     } 

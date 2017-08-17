720dbf13b124e8b861b679b7a0c79269de7e8d43
MaxiDave13 <maxidave13@gmail.com>
2017-08-17 15:23:32 +0200

    Començant a automatitzar tasu ques de gestio MySQL

--- Sfod/src/sfod/FXMLDatabaseControler.java
+++ Sfod/src/sfod/FXMLDatabaseControler.java
@@ -1,7 +1,5 @@
 package sfod;
 
-import javafx.beans.property.*;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -9,15 +7,10 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
@@ -25,18 +18,12 @@ import javafx.stage.Stage;
 public class FXMLDatabaseControler implements Initializable {
     
     @FXML
-    private Label sqlTitol;
-    
-    @FXML
     private Label sqlError;
     
     @FXML
     private TextArea sqlComanda;
-    
-    @FXML
-    private Button sqlExecutar;
 
-    private Connection conexio;
+    private final Connection conexio;
     
     public FXMLDatabaseControler(Connection con){
         conexio= con;
--- Sfod/src/sfod/SQL.java
+++ Sfod/src/sfod/SQL.java
@@ -3,6 +3,9 @@ package sfod;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
 
 public abstract class SQL {
     private static final String server= "jdbc:mysql://sql11.freemysqlhosting.net/sql11186567";
@@ -13,9 +16,26 @@ public abstract class SQL {
     public static Connection connectar(String user, String pass) throws SQLException, ClassNotFoundException{
         Connection conexion;
         Class.forName(driver);
-        user= SQL.user;
-        pass= SQL.pass;
+        if("maxi".equals(user)) user= SQL.user;
+        if("111".equals(pass)) pass= SQL.pass;
         conexion=DriverManager.getConnection(server, user, pass);
         return conexion;
-    } 
+    }
+    
+    private static String escriureCamp(Camp camp){
+        String resultat= camp.obtNom()+" "+camp.obtTipus();
+        if(camp.teEspai()) resultat+= "("+camp.obtEspai()+") ";
+        else resultat+=" ";
+        if(!camp.potSerNull()) resultat+= "not NULL";
+        
+    }
+    
+    public static void crearTaula(Connection conn, String nom, List<Camp> camps) throws SQLException{
+        Statement stm= conn.createStatement();
+        String sql= "CREATE TABLE "+nom+" (";
+        Iterator it= camps.iterator();
+        while(it.hasNext()){
+            sql+= escriureCamp(it.next());
+        }
+    }
 }
